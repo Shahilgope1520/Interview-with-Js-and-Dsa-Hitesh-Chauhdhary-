8de793491549cc7c326c68b0bddae7ec84559500
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-06-13 08:27:11 +0530

    changes for  insertInBetween

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -11,52 +11,42 @@ class LinkedList {
 }
 
 // Insert at beginning
-LinkedList.prototype.insertAtBeginning  =function(data){
-    const newNode = new Node(data)
-    LinkedList.head = newNode
-}
+LinkedList.prototype.insertAtBeginning = function (data) {
+  const newNode = new Node(data);
+  LinkedList.head = newNode;
+};
 
 // insert at end
-LinkedList.prototype.insertAtEnd  =function(data){
-    const newNode = new Node(data)
-    if(!this.head){
-        this.head = newNode;
-        return;
-    }
-    let last = this.head;
+LinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = new Node(data);
+  if (!this.head) {
+    this.head = newNode;
+    return;
+  }
+  let last = this.head;
 
-    while(last.next){
-        last= last.next
-    }
-    last.next = newNode
-}
+  while (last.next) {
+    last = last.next;
+  }
+  last.next = newNode;
+};
 
-LinkedList.prototype.insertInBetween  =function(data,index){
-    
-    if(!this.head){
-        this.head = newNode;
-        return;
-    }
-    let location = this.head;
-
-    for (let i = 0; i <= index; i++) {
-        location = location.next
-        if(i===index){
-            const newNode = new Node(data,location.next);
-            location=newNode
-            return;
-        }
-        
-    }
-
-    // while(last.next){
-    //     last= last.next
-    // }
-    // last.next = newNode
-}
-const myLinkList = new LinkedList()
-myLinkList.insertAtEnd(10)
-myLinkList.insertAtEnd(20)
-myLinkList.insertAtEnd(30)
-myLinkList.insertInBetween(15)
-console.log('myLinkList', myLinkList)
+LinkedList.prototype.insertInBetween = function (data, prevNode) {
+  console.log('prevNode', prevNode)
+  if (!prevNode) {
+    console.log("prevNode can't be null");
+    return;
+  }
+  const newNode = new Node(data, prevNode.next);
+  console.log('newNode', newNode)
+  prevNode.next = newNode;
+};
+const myLinkList = new LinkedList();
+myLinkList.insertAtEnd(10);
+myLinkList.insertAtEnd(20);
+myLinkList.insertAtEnd(30);
+myLinkList.insertAtEnd(40);
+myLinkList.insertInBetween(25,
+  myLinkList?.head?.next 
+);
+console.log("myLinkList", myLinkList);
