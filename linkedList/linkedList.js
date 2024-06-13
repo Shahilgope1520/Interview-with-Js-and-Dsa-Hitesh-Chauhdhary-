28201056787c5d727849b17e873725e9f3363a51
ShahilVrize <146432549+ShahilVrize@users.noreply.github.com>
2024-06-13 22:23:26 +0530

    delete first and last node

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -31,22 +31,49 @@ LinkedList.prototype.insertAtEnd = function (data) {
   last.next = newNode;
 };
 
+// insert at any position
 LinkedList.prototype.insertInBetween = function (data, prevNode) {
-  console.log('prevNode', prevNode)
+  console.log("prevNode", prevNode);
   if (!prevNode) {
     console.log("prevNode can't be null");
     return;
   }
   const newNode = new Node(data, prevNode.next);
-  console.log('newNode', newNode)
+  console.log("newNode", newNode);
   prevNode.next = newNode;
 };
+
+//delete first node
+LinkedList.prototype.deleteFirstNode = function () {
+  if (!this.head) {
+    console.log("Nothing to delete");
+    return;
+  }
+  this.head = this.head.next;
+};
+
+//delete last node
+LinkedList.prototype.deleteLastNode = function () {
+  if (!this.head) {
+    return;
+  }
+  if (!this.head.next) {
+    this.head = null;
+  }
+  let secondLast = this.head;
+  while (secondLast.next.next) {
+    secondLast = secondLast.next;
+  }
+  secondLast.next = null;
+};
+
 const myLinkList = new LinkedList();
+myLinkList.deleteFirstNode();
 myLinkList.insertAtEnd(10);
 myLinkList.insertAtEnd(20);
 myLinkList.insertAtEnd(30);
 myLinkList.insertAtEnd(40);
-myLinkList.insertInBetween(25,
-  myLinkList?.head?.next 
-);
+myLinkList.insertInBetween(25, myLinkList?.head?.next);
+myLinkList.deleteFirstNode();
+myLinkList.deleteLastNode();
 console.log("myLinkList", myLinkList);
